85b5151a2d37bf54948ad19a5de4ade7a5e7cf3b
Dan Poltawski <dan@moodle.com>
2017-04-27 17:01:56 +0100

    Refactor php docker files into more manageable chunks

---
+++ moodle-php71/setup-php-extensions.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing apt depdencies"
+
+
+apt-get update
+apt-get install -y \
+    gettext \
+    libcurl4-openssl-dev \
+    libpq-dev \
+    libmysqlclient-dev \
+    libldap2-dev \
+    libxslt-dev \
+    libxml2-dev \
+    libicu-dev \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    libmcrypt-dev \
+    libmemcached-dev \
+    zlib1g-dev \
+    libpng12-dev \
+    libaio1 \
+    unzip \
+    ghostscript \
+    locales
+
+#RUN apt-get install -y locales && echo "en_US.UTF-8 UTF-8\nen_AU.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen en_US.UTF-8 en_AU.UTF-8
+
+echo "Installing php extensions"
+docker-php-ext-install -j$(nproc) \
+    intl \
+    mysqli \
+    opcache \
+    pgsql \
+    soap \
+    xsl \
+    xmlrpc \
+    zip
+
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-install -j$(nproc) gd
+
+docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
+docker-php-ext-install -j$(nproc) ldap
+
+pecl install solr memcached redis apcu igbinary
+docker-php-ext-enable solr memcached redis apcu igbinary
+
+echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
