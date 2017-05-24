944828bac81bf61331e53c1694af04c05e320e65
Dan Poltawski <dan@moodle.com>
2017-05-24 17:04:14 +0100

    Completely rework the repo layout
    * Using a series of docker compose templates which can be cobined
    * Added some travis tests for each db driver

---
+++ config.docker-template.php
@@ -0,0 +1,38 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('MOODLE_DOCKER_DBTYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'db';
+$CFG->dbname    = getenv('MOODLE_DOCKER_DBNAME');
+$CFG->dbuser    = getenv('MOODLE_DOCKER_DBUSER');
+$CFG->dbpass    = getenv('MOODLE_DOCKER_DBPASS');
+$CFG->prefix    = 'a_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+);
+
+$CFG->wwwroot   = 'http://localhost:8000';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+$CFG->directorypermissions = 0777;
+
+$CFG->phpunit_dataroot  = '/var/www/moodledata/phpunit';
+$CFG->phpunit_prefix = 't_';
+
+$CFG->behat_wwwroot   = 'http://php71';
+$CFG->behat_dataroot  = '/var/www/moodledata/behat';
+$CFG->behat_prefix = 'b_';
+$CFG->behat_profiles = array(
+    'default' => array(
+        'browser' => 'firefox',
+        'wd_host' => 'http://selenium:4444/wd/hub',
+        'tags' => '~@_file_upload',
+    ),
+);
+
+define('PHPUNIT_LONGTEST', true);
+require_once(__DIR__ . '/lib/setup.php');
