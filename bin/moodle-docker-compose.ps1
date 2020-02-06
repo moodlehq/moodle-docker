8122ce84dff4a197d42f985c8a6e7177581e45bf
markv <vhmark@gmail.com>
2020-02-05 21:28:13 -0800

    MOODLE_DOCKER_WEB_PORT and MOODLE_DOCKER_SELENIUM_VNC_PORT to control both bind_ip and port; Restore missing MOODLE_DOCKER_PHPUNIT_EXTERNAL_SERVICES

--- bin/moodle-docker-compose.ps1
+++ bin/moodle-docker-compose.ps1
@@ -1,13 +1,13 @@
-Set-PSDebug -Trace 1
+#Set-PSDebug -Trace 1
 
-IF (!$ENV:MOODLE_DOCKER_WWWROOT) {
+if (!$ENV:MOODLE_DOCKER_WWWROOT) {
     echo Error: MOODLE_DOCKER_WWWROOT is not set or not an existing directory
-    EXIT 1
+    exit 1
 }
 
-IF (!$ENV:MOODLE_DOCKER_DB) {
+if (!$ENV:MOODLE_DOCKER_DB) {
     echo Error: MOODLE_DOCKER_DB is not set
-    EXIT 1
+    exit 1
 }
 
 $BASEDIR='$pwd'
@@ -19,12 +19,12 @@ $DOCKERCOMPOSE="docker-compose -f $BASEDIR\base.yml"
 $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\service.mail.yml"
 
 # PHP Version.
-IF (!$ENV:MOODLE_DOCKER_PHP_VERSION) {
+if (!$ENV:MOODLE_DOCKER_PHP_VERSION) {
     $ENV:MOODLE_DOCKER_PHP_VERSION=7.2
 }
 
 # Database flavour
-IF ($ENV:MOODLE_DOCKER_DB -ne 'pgsql') {
+if ($ENV:MOODLE_DOCKER_DB -ne 'pgsql') {
     $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\db.$ENV:MOODLE_DOCKER_DB.yml"
 }
 
@@ -35,33 +35,52 @@ if (Test-Path $filename) {
 }
 
 # Selenium browser
-IF ($ENV:MOODLE_DOCKER_BROWSER -ne "") {
-    IF ("$ENV:MOODLE_DOCKER_BROWSER" -ne "firefox") {
+if ($ENV:MOODLE_DOCKER_BROWSER -ne "") {
+    if ("$ENV:MOODLE_DOCKER_BROWSER" -ne "firefox") {
         $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\selenium.$ENV:MOODLE_DOCKER_BROWSER.yml"
     }
 }
 
+if ($ENV:MOODLE_DOCKER_PHPUNIT_EXTERNAL_SERVICES -ne "") {
+    $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\phpunit-external-services.yml"
+}
+
+# External services
 IF ($ENV:MOODLE_DOCKER_PHPUNIT_EXTERNAL_SERVICES -ne "") {
     $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\phpunit-external-services.yml"
 }
 
 # Webserver host
-IF (!$ENV:MOODLE_DOCKER_WEB_HOST) {
+if (!$ENV:MOODLE_DOCKER_WEB_HOST) {
     $ENV:MOODLE_DOCKER_WEB_HOST='localhost'
 }
 
 # Webserver port
-IF (!$ENV:MOODLE_DOCKER_WEB_PORT) {
+if (!$ENV:MOODLE_DOCKER_WEB_PORT) {
     $ENV:MOODLE_DOCKER_WEB_PORT=8000
 }
 
-IF ($ENV:MOODLE_DOCKER_WEB_PORT -ne 0) {
+$has_ip = $ENV:MOODLE_DOCKER_WEB_PORT -match '^.*:\d*$'
+if ($has_ip -Or ([Int32]::TryParse($ENV:MOODLE_DOCKER_WEB_PORT,[ref]"") -And $ENV:MOODLE_DOCKER_WEB_PORT -gt 0) {
+    # If no bind ip has been configured (bind_ip:port), default to 127.0.0.1
+	if(!$has_ip) {
+		$ENV:MOODLE_DOCKER_WEB_PORT = '127.0.0.1:'+$ENV:MOODLE_DOCKER_WEB_PORT
+	}
+
     $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\webserver.port.yml"
 }
 
-IF (!$ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT) {
-    $ENV:MOODLE_DOCKER_SELENIUM_SUFFIX=''
-} ELSE {
+# Selenium VNC port
+$ENV:MOODLE_DOCKER_SELENIUM_SUFFIX=""
+$has_ip = $ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT -match '^.*:\d*$'
+if ($has_ip -Or ([Int32]::TryParse($ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT,[ref]"") -And $ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT -gt 0) {
+	$ENV:MOODLE_DOCKER_SELENIUM_SUFFIX=''
+
+	# If no bind ip has been configured (bind_ip:port), default to 127.0.0.1
+    if(!$has_ip) {
+		$ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT = '127.0.0.1:'+$ENV:MOODLE_DOCKER_SELENIUM_VNC_PORT
+	}
+} else {
     $ENV:MOODLE_DOCKER_SELENIUM_SUFFIX='-debug'
     $DOCKERCOMPOSE="$DOCKERCOMPOSE -f $BASEDIR\selenium.debug.yml"
 }
