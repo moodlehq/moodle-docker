80787e397ed05b4d1f714bc1d0200ea82d71f2a9
markv <vhmark@gmail.com>
2020-02-08 19:22:19 -0800

    Add vars to stop and/or destroy

--- start.ps1
+++ start.ps1
@@ -4,7 +4,9 @@
 Disable-VMIntegrationService -VMName DockerDesktopVM -Name "Time Synchronization"
 Enable-VMIntegrationService -VMName DockerDesktopVM -Name "Time Synchronization"
 
-$MANUAL_NOT_BEHAT=1
+$MANUAL_NOT_BEHAT=0
+$DO_STOP=0
+$DO_STOP_AND_DESTROY=0
 
 #$ENV:MOODLE_DOCKER_WEB_PORT='127.0.0.1:8001'
 $ENV:MOODLE_DOCKER_BROWSER='chrome'
@@ -53,11 +55,16 @@ if($MANUAL_NOT_BEHAT) {
 	# Run behat tests
 	#./bin/moodle-docker-compose.ps1 exec -u www-data webserver php admin/tool/behat/cli/run.php --tags=@auth_manual
 	./bin/moodle-docker-compose.ps1 exec -u www-data webserver php admin/tool/behat/cli/run.php --tags=@block_integrityadvocate
+}
 
+IF($DO_STOP) {
 	# Stop w/o destroying the container
-	#./bin/moodle-docker-compose.ps1 stop #OR# docker stop $(docker ps --quiet --filter='name=moodle-')
-	
+	./bin/moodle-docker-compose.ps1 stop #OR# the below line
+	#docker stop $(docker ps --quiet --filter='name=moodle-')
+}
+
+IF($DO_STOP_AND_DESTROY) {
 	# Stop and destroy the container
-	#./bin/moodle-docker-compose.ps1 down #OR# the below line
+	./bin/moodle-docker-compose.ps1 down #OR# the below line
 	#docker stop $(docker ps --quiet --filter='name=moodle-'); docker rm $(docker ps --all --quiet --filter='name=moodle-')
 }
