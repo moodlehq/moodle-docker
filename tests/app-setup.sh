d830b6a8602ccd59c9597e1960fc94e994e7d6ca
Noel De Martin <noel@moodle.com>
2021-07-20 12:32:43 +0200

    MOBILE-3738 app: Update with latest versions

--- tests/app-setup.sh
+++ tests/app-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+initcmd="bin/moodle-docker-compose exec -T webserver php admin/tool/behat/cli/init.php"
 
 export MOODLE_DOCKER_WWWROOT="${basedir}/moodle"
 export MOODLE_DOCKER_BROWSER="chrome"
@@ -9,14 +10,8 @@ if [ "$SUITE" = "app-development" ];
 then
     export MOODLE_DOCKER_APP_PATH="${basedir}/app"
 
-    if [[ $RUNTIME = ionic5 ]];
-    then
-        git clone --branch "ionic5" --depth 1 git://github.com/moodlehq/moodleapp $basedir/app
-        git clone --branch "ionic5" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
-    else
-        git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodleapp $basedir/app
-        git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
-    fi
+    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodleapp $basedir/app
+    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
 
     if [[ $RUNTIME = ionic5 ]];
     then
@@ -25,18 +20,11 @@ then
         docker run --volume $basedir/app:/app --workdir /app node:11 npm run setup
         docker run --volume $basedir/app:/app --workdir /app node:11 npm ci
     fi
-
-    initcmd="bin/moodle-docker-compose exec -T webserver php admin/tool/behat/cli/init.php"
 elif [ "$SUITE" = "app" ];
 then
-    if [[ $RUNTIME = ionic5 ]];
-    then
-        git clone --branch "ionic5" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
-    else
-        git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
-    fi
+    appversion=`echo $MOODLE_DOCKER_APP_VERSION | grep -E -o "[0-9]+\.[0-9]+\.[0-9]"`
 
-    initcmd="bin/moodle-docker-compose exec -T webserver php admin/tool/behat/cli/init.php"
+    git clone --branch "v$appversion" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
 else
     echo "Error, unknown suite '$SUITE'"
     exit 1
