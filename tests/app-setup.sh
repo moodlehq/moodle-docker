3c709aa31452f1cb773b31e05c49cafa46f54c98
Alfonso Salces <alfonso.salces@moodle.com>
2022-04-11 15:03:23 +0200

    208 app-setup: update git clone protocol

--- tests/app-setup.sh
+++ tests/app-setup.sh
@@ -10,8 +10,8 @@ if [ "$SUITE" = "app-development" ];
 then
     export MOODLE_DOCKER_APP_PATH="${basedir}/app"
 
-    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodleapp $basedir/app
-    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
+    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodleapp $basedir/app
+    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
 
     if [[ $RUNTIME = ionic5 ]];
     then
@@ -24,7 +24,7 @@ elif [ "$SUITE" = "app" ];
 then
     appversion=`echo $MOODLE_DOCKER_APP_VERSION | grep -E -o "[0-9]+\.[0-9]+\.[0-9]"`
 
-    git clone --branch "v$appversion" --depth 1 git://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
+    git clone --branch "v$appversion" --depth 1 https://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
 else
     echo "Error, unknown suite '$SUITE'"
     exit 1
