1e4a4977c3c996eb610465bc360e35b4c6843be9
Neill Magill <neill.magill@nottingham.ac.uk>
2023-06-06 11:58:15 +0100

    Update location of Moodle app behat tests steps

--- tests/app-setup.sh
+++ tests/app-setup.sh
@@ -11,7 +11,7 @@ then
     export MOODLE_DOCKER_APP_PATH="${basedir}/app"
 
     git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodleapp $basedir/app
-    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
+    git clone --branch "main" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
 
     if [[ $RUNTIME = ionic5 ]];
     then
@@ -22,9 +22,7 @@ then
     fi
 elif [ "$SUITE" = "app" ];
 then
-    appversion=`echo $MOODLE_DOCKER_APP_VERSION | grep -E -o "[0-9]+\.[0-9]+\.[0-9]"`
-
-    git clone --branch "v$appversion" --depth 1 https://github.com/moodlehq/moodle-local_moodlemobileapp $basedir/moodle/local/moodlemobileapp
+    git clone --branch "latest" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
 else
     echo "Error, unknown suite '$SUITE'"
     exit 1
