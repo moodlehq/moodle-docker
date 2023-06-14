a55ad3a91caa5413dc2c503620e7e0a3310bfeb9
Neill Magill <neill.magill@nottingham.ac.uk>
2023-06-14 11:10:04 +0100

    Update Moodle app tests
    The tests now simply check that the app can be started for both the lastest and development versions of the app work properly.
    
    The Ionic3 versions are now well out of support.
    
    It is likely most users would only be using these two versions of the app.

--- tests/app-setup.sh
+++ tests/app-setup.sh
@@ -10,8 +10,8 @@ if [ "$SUITE" = "app-development" ];
 then
     export MOODLE_DOCKER_APP_PATH="${basedir}/app"
 
-    git clone --branch "v$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodleapp $basedir/app
-    git clone --branch "main" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
+    git clone --branch "$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodleapp $basedir/app
+    git clone --branch "$MOODLE_DOCKER_APP_VERSION" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
 
     if [[ $RUNTIME = ionic5 ]];
     then
@@ -22,11 +22,21 @@ then
     fi
 elif [ "$SUITE" = "app" ];
 then
-    git clone --branch "latest" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
+    isdevelop=`echo $MOODLE_DOCKER_APP_VERSION | grep -E -o "(next)|(latest)"`
+
+    branch="latest"
+    if [ "$isdevelop" = "next" ];
+    then
+        branch="main"
+    fi
+
+    git clone --branch "$branch" --depth 1 https://github.com/moodlehq/moodle-local_moodleappbehat $basedir/moodle/local/moodleappbehat
 else
     echo "Error, unknown suite '$SUITE'"
     exit 1
 fi
+
+cp $basedir/assets/appbehattests/app.feature $basedir/moodle/local/moodleappbehat/tests/behat/app.feature
 
 echo "Pulling docker images"
 $basedir/bin/moodle-docker-compose pull
