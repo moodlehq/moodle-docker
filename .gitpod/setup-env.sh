ccfd87fbd2e66898b7fb0e841612e703e846c7fb
Sara Arjona <sara@moodle.com>
2024-05-06 15:55:55 +0200

    Initialise gitpod with data when DATAFILE is given

--- .gitpod/setup-env.sh
+++ .gitpod/setup-env.sh
@@ -10,3 +10,9 @@ fi
 
 # Clone Moodle repository.
 cd "${GITPOD_REPO_ROOT}" && git clone --branch "${MOODLE_BRANCH}" --single-branch "${MOODLE_REPOSITORY}" moodle
+
+# Download the data file (if given). It will be used to generate some data.
+if [ -n "$DATAFILE" ];
+then
+    wget -O moodle/admin/tool/generator/tests/fixtures/gitpod-basic-scenario.feature "${DATAFILE}"
+fi
