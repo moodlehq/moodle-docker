1b015665fe83444d59ab352f63295ee03e0532d9
Nicolas Dalpe <55551039+nicolas-dalpe@users.noreply.github.com>
2025-03-07 15:08:53 -0500

    MECONTRACK-245 - Add xDebug to Docker on many instances (#17)
    * MECONTRACK-245 - Added xDebug config file in assets.
    
    * MECONTRACK-245 - Added k1many2.sh which introduces the build_instances function to add xDebug in the build process.
    
    * MECONTRACK-245 - Add a function to reset config files after sutting down or destroying containers.
    
    * MECONTRACK-245 - Created k1many3.sh to implement xdebug with --stop/--start.
    
    * MECONTRACK-245 - Modify xDebug log file path.
    
    * MECONTRACK-245 - Added k1many3.sh to add xDebug with --build and --start.
    
    * MECONTRACK-245 - On going task. Added xdebug to --start and --stop.
    
    * MECONTRACK-245 Update to xdebug on multiple sites.
    
    * MECONTRACK-245 - Support xDebug when --start & --stop.
    
    Add Excimer support.
    
    * MECONTRACK-245 - Rewrite the doc ( --help ).
    
    * MECONTRACK-245 - Removed K1many2.sh.
    
    * MECONTRACK-245 - Removed K1many.sh.
    
    * MECONTRACK-245 - Renamed k1many3.sh to k1many.sh.

--- k1many.sh
+++ k1many.sh
@@ -1,57 +1,138 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Define colors for various the output messages.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
 
 help_messages () {
-    echo "Usage: sh k1many.sh [option] [folder1] [folder2] [folder2]"
-    echo "Script that can start multiple sites."
     echo
-    echo "--build       Start the site and initialize the sites."
-    echo "--destroy     Stop the sites. Destroy all data"
-    echo "--help        display this message"
-    echo "--restart     Restart all running sites."
-    echo "--start       Start all stopped sites."
-    echo "--stop        Stop all running sites."
-
-    echo "{folder1}     -folder pointing to first Moodle site (Mandatory)"
-    echo "{folder2}     -folder pointing to second Moodle site (optional)"
-    echo "{folder3}     -folder pointing to third Moodle site (optional)"
-    echo "
-        Examples:
-            sh k1many.sh --build M405            # Start a site with the folder in docker/M405 folder
-            sh k1many.sh --build M405 M401 MT    # Start three sites with based in docker/M405, docker/M401, docker/MT
-            sh k1many.sh --start                 # Start all stopped sites
-            sh k1many.sh --stop                  # Stop all stopped sites
-            sh k1many.sh --destroy               # Stop and destroy sites
-        "
+    echo
+    echo -e "${GREEN}Usage:${NC}"
+    echo "    Script that can start multiple Moodle instances."
+    echo "    ./k1many.sh option folder1 [folder2] [folder3]"
+    echo
+    echo -e "${GREEN}Options:${NC}"
+    echo "    --build       Create and start the containers for the passed folders."
+    echo "    --destroy     Stop and remove all running containers. All data is lost."
+    echo "    --help        Display this message."
+    echo "    --restart     Restart all running containers."
+    echo "    --start       Start all stopped containers. Needs folder(s) path as well. See examples."
+    echo "    --stop        Stop all running containers."
+    echo
+    echo -e "${GREEN}Folders:${NC}"
+    echo "    folder1       Folder pointing to first Moodle instance (Mandatory)."
+    echo "    [folder2]     Folder pointing to second Moodle instance (optional)."
+    echo "    [folder3]     Folder pointing to third Moodle instance (optional)."
+    echo
+    echo -e "${GREEN}Examples:${NC}"
+    echo "    Start three Moodle instances. Path starts at /home/username/projects/[folder]."
+    echo "    In this example, the fist Moodle instance will be in /home/username/projects/M405."
+    echo "    ./k1many.sh --build M405 M401 MT"
+    echo
+    echo "    Start all Moodle instances passed in script parameters. Same as --build."
+    echo "    ./k1many.sh --start folder1 [folder2] [folder3]"
+    echo
+    echo "    Stops all running containers."
+    echo "    ./k1many.sh --stop"
+    echo
+    echo "    Stops and removes all running containers. All data is lost."
+    echo "    ./k1many.sh --destroy"
+    echo
+    echo
 }
 
 # Decorator to output information message to the user.
 # Takes 1 param, then message to be displayed.
 info_message() {
-    CYAN='\033[0;36m'
-    NC='\033[0m' # No Color
     echo
-    echo "${CYAN}$1${NC}"
+    echo -e "${CYAN}$1${NC}"
     echo
 }
 
 # Decorator to output error message to the user.
 # Takes 1 param, then message to be displayed.
 error_message() {
-    RED='\033[0;31m'
-    NC='\033[0m' # No Color
     echo
-    echo "${RED}$1${NC}"
+    echo -e "${RED}$1${NC}"
     echo
 }
 
 start_server() {
-     # Start the server
-     bin/moodle-docker-compose up -d
-     # Sleep for 6 seconds to allow database to come up
-     sleep 6
-     # Just in case there is still some latency.
-     bin/moodle-docker-wait-for-db
-     return 0
+    # Start the server
+    bin/moodle-docker-compose up -d
+
+    # Sleep for 6 seconds to allow database to come up
+    # sleep 6
+
+    # Just in case there is still some latency.
+    bin/moodle-docker-wait-for-db
+}
+
+start_instances() {
+
+    # Project name.
+    projectname=$1
+
+    # Starts the container and wait for DB.
+    start_server
+
+    info_message "${projectname} is available at http://localhost:${MOODLE_DOCKER_WEB_PORT}"
+}
+
+# Function to group the commands to build the each instances.
+# The params are validated before this function is called.
+build_instances() {
+    # Project name (folder that contains the Moodle files)
+    projectname=$1
+
+    # Full path of the Moodle folder.
+    folder=$2
+
+    # xDebug port. Must be different for each instances.
+    # xdebugport=$3
+
+    # Reset the xDebug config file from previous boot.
+    # We reset it to set the client port back 9003 to
+    # make sure the sed will find and replace it with
+    # the new client port.
+    # git checkout assets/php/docker-php-ext-xdebug.ini
+
+    # Change xDebug port for this instance.
+    # sed -i -e 's/xdebug.client_port=9003/xdebug.client_port='"${xdebugport}"'/g' assets/php/docker-php-ext-xdebug.ini
+
+    # Starts the container and wait for DB.
+    start_server
+
+    # This is a Moodle admin plugin that provides developers with insights into
+    # not only what pages in your site are slow, but why.
+    # It uses the the Excimer sampling php profiler to so.
+    bin/moodle-docker-compose exec webserver pecl install excimer
+    bin/moodle-docker-compose exec webserver docker-php-ext-enable excimer
+
+    # Install Moodle.
+    bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
+
+    # Set the session cookie to avoid login problem between instances.
+    bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
+
+    # Install xdebug extention in the new webserser.
+    # If already installed, the install will just fail.
+    bin/moodle-docker-compose exec webserver pecl install xdebug
+
+    # Enable XDebug extension in Apache and restart the webserver container
+    bin/moodle-docker-compose exec webserver docker-php-ext-enable xdebug
+    bin/moodle-docker-compose restart webserver
+
+    info_message "${folder} is available at http://localhost:${MOODLE_DOCKER_WEB_PORT}"
+}
+
+# This function resets all config files used during boot of project.
+# Should be used when --destroy containers.
+reset_config_files() {
+    rm local.yml
+    git checkout assets/php/docker-php-ext-xdebug.ini
 }
 
 exists_in_list() {
@@ -67,146 +148,160 @@ exists_in_list() {
     return 0
 }
 
-# Variables
+validate_folder_path() {
+    if [ ! -d "${1}" ]; then
+        error_message "${1} is not a valid folder."
+        help_messages
+        exit 1
+    fi
+}
+
+###################
+### Validations ###
+if [ $# -eq 0 ]; then
+    error_message "No arguments supplied. See help message below."
+    help_messages
+    exit 1
+fi
+
+if [ $# -lt 1 ] || [ $# -gt 4 ]; then
+    error_message "Invalid number of arguments passed in. Must be between 1 and 4 arguments."
+    help_messages
+    exit 1
+fi
+
+# Validate the switch passed to the script.
+list_of_options="--build --destroy --help --reboot --stop --start --restart"
+SWITCH=$1
+if  exists_in_list "$list_of_options" " " $SWITCH; then
+    error_message "Invalid option $SWITCH."
+    help_messages
+    exit 1
+fi
+
+# Display help message if the --help switch is present.
+if [ "$SWITCH" = "--help" ]; then
+    help_messages
+    exit 0
+fi
+
+########################
+### Global variables ###
 # MOODLE_DOCKER_DB          - database used by Moodle - default maria db
 # MOODLE_DOCKER_WWWROOT     - folder where the Moodle code is located;
 # MOODLE_DOCKER_PORT        - port (default 8000);
 # MOODLE_DOCKER_PHP_VERSION - php version used in Moodle - default 8.1;
 # COMPOSE_PROJECT_NAME      - Docker project name - used to identify sites;
 
-if [ $# -eq 0 ];  then
-    error_message "No arguments supplied"
-    help_messages
-    exit 1
-fi
+# We always use Maria DB for all our Moodle.
+export MOODLE_DOCKER_DB=mariadb
 
-if [ $# -lt 1 ] || [ $# -gt 4 ] ;  then
-    error_message "Invalid number of arguments passed in. Must be between 1 and 4 arguments"
-    help_messages
-    exit 1
-fi
-
-list_of_options="--build  --destroy --help --reboot --stop --start --restart"
+# Current working dir.
+# Typically gives /home/username/projects.
 cwd=$( dirname "$PWD" )
-count=0
-for var in "$@"
+
+# Moodle Docker Port.
+# AKA which port the webserver container will listen to.
+moodleDockerPort=(8000 1234 5678)
+
+# xDebug ports
+# Each containers must have a their own port.
+xdebugPort=(9003 9004 9005)
+
+# Use the multiple instances local.yml file.
+cp local.yml_many local.yml
+
+################################################
+### Process swithes that don't need options. ###
+case $SWITCH in
+    "--destroy")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to shutdown."
+            exit 1
+        fi
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+        reset_config_files
+        info_message "All containers shut down and removed."
+        exit 1
+        ;;
+    "--restart")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to reboot."
+            exit 1
+        fi
+        docker restart $(docker ps -q)
+        info_message "All sites restarted."
+        exit 1
+        ;;
+    "--stop")
+        if ! docker ps | grep -q 'moodlehq'; then
+            info_message "No containers running. Nothing to stop."
+            exit 1
+        fi
+        docker stop $(docker ps -q)
+        # reset_config_files
+        info_message "All sites stopped."
+        exit 1
+        ;;
+esac
+
+#############################################
+### Process swithes that require options. ###
+
+# Ignore first parm passed to the script.
+# Skip the --switch to cycle the folders in argument.
+shift
+
+iteratior=0
+
+# Iterate the passed folders.
+for i in "$@"
 do
-    count=$((count+1))
-    if [ "$count" -eq 1 ]; then
-        SWITCH=${var}
-        # Check to see if the list of arguments is valid.
-        if  exists_in_list "$list_of_options" " " $SWITCH;  then
-            error_message "Invalid option $SWITCH."
-            help_messages
-            exit 1
-        fi
-        # Check for any swithes that don't need options.
-        case $SWITCH in
-           "--build")
-                if [ -n "$(docker ps -f "name=${var}-webserver-1" -f "status=running" -q )" ]; then
-                    echo "The first site is already running!. It cannot be re-initialized."
-                    exit 1
-                fi
-                # Do the basics to start the site.
-                export MOODLE_DOCKER_DB=mariadb
-                export COMPOSE_PROJECT_NAME=site1
-                cp local.yml_many local.yml
-                ;;
-           "--help")
-              help_messages
-              exit 1
-              ;;
-            "--destroy")
-                if ! docker ps | grep -q 'moodlehq'; then
-                   info_message "No containers running. Nothing to shutdown."
-                   exit 1
-                fi
-                docker stop $(docker ps -a -q)
-                docker rm $(docker ps -a -q)
-                info_message "All containers shut down and removed."
-                exit 1
-                ;;
-             # Restart
-            "--restart")
-                if ! docker ps | grep -q 'moodlehq'; then
-                     info_message "No containers running. Nothing to reboot."
-                     exit 1
-                fi
-                docker restart $(docker ps -q)
-                info_message "All sites restarted."
-                exit 1
-                ;;
-            # Start
-            "--start")
-                if [ -n "$(docker ps -f "name=site1-webserver-1" -f "status=running" -q )" ]; then
-                     info_message "Sites are already running."
-                     exit 1
-                fi
-                docker start $(docker ps -a -q -f status=exited)
-                info_message "All sites started."
-                exit 1
-                ;;
-            # Stop
-            "--stop")
-                if ! docker ps | grep -q 'moodlehq'; then
-                   info_message "No containers running. Nothing to stop."
-                   exit 1
-               fi
-               docker stop $(docker ps -q)
-               info_message "All sites stopped."
-               exit 1
-               ;;
+    # Folder passed in args to the script.
+    argfolder="$i"
 
-        esac
-    else
-        # Used to name the project according to the folder name
-        # which makes it easier to recognize who is who in Docker.
-        projectname="${var}"
+    # Full system path of the Moodle instance.
+    validate_folder_path "${cwd}/${argfolder}"
+    fullfolderpath="${cwd}/${argfolder}"
+    export MOODLE_DOCKER_WWWROOT=${fullfolderpath}
 
-        # Full path of the folder containing the Moodle files.
-        folder="${cwd}/${var}"
-        if [ ! -d "${folder}" ]; then
-            echo "${folder} is not valid"
-            exit 1
-        fi
-
-        # Start the site
-        # Check the Moodle version. If its 4.5 then set php version to 8.3
-        export MOODLE_DOCKER_PHP_VERSION=8.1
-        moodlever=$(grep "$branch   = '405';" $folder/version.php)
-        if [ "$moodlever" ]; then
-           export MOODLE_DOCKER_PHP_VERSION=8.3
-        fi
-        export MOODLE_DOCKER_WWWROOT=${folder}
-        export COMPOSE_PROJECT_NAME=${projectname}
-        cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
-        case $count in
-           "2")
-               export MOODLE_DOCKER_WEB_PORT=8000
-               # Starts the container and wait for DB.
-               start_server
-               # Install Moodle.
-               bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-               # Set the session cookie to avoid login problem between instances.
-               bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-               info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-            ;;
-            "3")
-               export MOODLE_DOCKER_WEB_PORT=1234
-               start_server
-               bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-               bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-               info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-            ;;
-            "4")
-               export MOODLE_DOCKER_WEB_PORT=6789
-               start_server
-               bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="${projectname}" --shortname="${projectname}" --summary="${projectname}" --adminpass="test" --adminemail="admin@example.com"
-               bin/moodle-docker-compose exec webserver php admin/cli/cfg.php --name=sessioncookie --set="${projectname}"
-               info_message "${folder} site started - http://localhost:${MOODLE_DOCKER_WEB_PORT}"
-            ;;
-         esac
+    # Check the Moodle version. If its 4.5 then set php version to 8.3.
+    export MOODLE_DOCKER_PHP_VERSION=8.1
+    moodlever=$(grep "$branch   = '405';" $fullfolderpath/version.php)
+    if [ "$moodlever" ]; then
+        export MOODLE_DOCKER_PHP_VERSION=8.3
     fi
-done
 
-return
+    # Used to name the project according to the folder name
+    # which makes it easier to recognize who is who in Docker.
+    projectname="${argfolder}"
+    export COMPOSE_PROJECT_NAME=${projectname}
+
+    # Set the port for the webserver.
+    export MOODLE_DOCKER_WEB_PORT="${moodleDockerPort[$iteratior]}"
+
+    # Copy the config.php file to the Moodle folder.
+    cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+
+    case $SWITCH in
+        "--build")
+            if [ -n "$(docker ps -f "name=${projectname}-webserver-1" -f "status=running" -q )" ]; then
+                error_message "The first site is already running! It cannot be re-initialized."
+                exit 1
+            fi
+
+            # Build containers and install Moodle.
+            build_instances ${projectname} ${fullfolderpath} "${xdebugPort[$iteratior]}"
+            ;;
+        "--start")
+
+            # Start all Moodle instances.
+            start_instances ${projectname} "${xdebugPort[$iteratior]}"
+            ;;
+    esac
+
+    # Increment loop counter.
+    iteratior=$((iteratior+1))
+done
+exit 0
